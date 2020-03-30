3cb5e113a52562bb5b5b42898358f138dc2279f2
v_kenqzhang <v_kenqzhang@tencent.com>
2020-03-30 16:20:06 +0800

    [update]

--- src/algorithm/simple/question1.go
+++ src/algorithm/simple/question1.go
@@ -425,3 +425,24 @@ func (*Ref)MLE()  {
 	log.Println(l)
 }
 
+// https://leetcode-cn.com/problems/yuan-quan-zhong-zui-hou-sheng-xia-de-shu-zi-lcof/
+/**
+//https://blog.csdn.net/u011500062/article/details/72855826
+
+m : 每次删除第m个数字
+n : 一共有n个数字
+假设最后剩下的数字 在序列长度为n的数组中 索引为x
+找出每次删除的时候 n,m,x的关系即可解出方程
+ */
+func (*Ref)LR()  {
+	n, m := 70866, 116922
+
+	x  := 0
+	tn := 1
+	for i := 0;i <= n-1; i++ {
+		x = (x + m) % tn
+		tn++
+	}
+
+	log.Println(x)
+}
