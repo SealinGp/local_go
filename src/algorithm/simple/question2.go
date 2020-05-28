19ecad8bb10808fcfbd3e75048fe5babf73fa204
v_kenqzhang <v_kenqzhang@tencent.com>
2020-05-28 11:24:50 +0800

    [update]DecodeStr

--- src/algorithm/simple/question2.go
+++ src/algorithm/simple/question2.go
@@ -2,6 +2,8 @@ package simple
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 )
 
 //https://leetcode-cn.com/problems/subarray-sums-divisible-by-k/
@@ -18,4 +20,44 @@ func (*Ref)SubArrDivByK()  {
 		record[modules]++
 	}
 	fmt.Println(ans)
+}
+
+func (*Ref)DecodeString()  {
+	s        := "2[abc]3[cd]ef"
+
+	lastLeft := 0
+	F : for i, v := range s {
+		if v == '[' {
+			lastLeft = i
+		}
+		if v == ']' {
+			//找出被重复字符串的次数
+			numIndex := lastLeft-1
+			for {
+				if numIndex < 0 {
+					numIndex = 0
+					break
+				}
+				if s[numIndex] < '0' || s[numIndex] > '9' {
+					numIndex++
+					break
+				}
+				numIndex--
+			}
+
+			//找出被重复字符串
+			newV,_   := strconv.Atoi(s[numIndex:lastLeft])
+			res      := strings.Repeat(
+				s[lastLeft+1:i],
+				newV,
+			)
+
+			//字符串重新拼接
+			s = s[:numIndex] + res + s[i+1:]
+			lastLeft = 0
+			goto F
+		}
+	}
+
+	fmt.Println(s == "abcabccdcdcdef")
 }
