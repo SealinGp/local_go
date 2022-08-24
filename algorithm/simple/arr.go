7a77b85a3ebd5d9ee56863f8791e11668126ac2c
seazhang <seazhang@zego.im>
2022-08-24 22:51:09 +0800

    max profit

--- algorithm/simple/arr.go
+++ algorithm/simple/arr.go
@@ -2,6 +2,7 @@ package simple
 
 import (
 	"log"
+	"math"
 )
 
 func (r *Ref) BalanceIndex() {
@@ -28,4 +29,28 @@ func BalanceIndex(nums []int) int {
 	}
 
 	return -1
+}
+
+func (r *Ref) MaxProfit() {
+	log.Printf("%v", maxProfit([]int{7, 1, 5, 3, 6, 4}))
+}
+
+//对于每组i,j 求max(prices[i], prices[j]) j > i
+//假设历史最低价格minPrice
+//假设我们在第i天卖出股票
+//profit = prices[i] - minPrice
+func maxProfit(prices []int) int {
+	minPrice := math.MaxInt
+	maxProfit := 0
+
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrice {
+			minPrice = prices[i]
+		} else if prices[i]-minPrice > maxProfit {
+			maxProfit = prices[i] - minPrice
+		}
+
+	}
+
+	return maxProfit
 }
