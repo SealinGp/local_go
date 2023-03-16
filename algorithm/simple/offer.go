3349f6abe83452b330231f20d9192161c7ce73a6
SealinGp <sealingp@163.com>
2023-03-16 21:58:46 +0800

    feat:verify post order
    Signed-off-by: SealinGp <sealingp@163.com>

--- algorithm/simple/offer.go
+++ algorithm/simple/offer.go
@@ -4,6 +4,40 @@ import (
 	"log"
 )
 
+func verifyPostorder(postorder []int) bool {
+	if len(postorder) == 0 {
+		return true
+	}
+
+	root := postorder[len(postorder)-1]
+
+	i := 0
+	for ; i < len(postorder)-1; i++ {
+		if postorder[i] > root {
+			break
+		}
+	}
+
+	j := i
+	for ; j < len(postorder)-1; j++ {
+		if postorder[j] < root {
+			return false
+		}
+	}
+
+	left := true
+	if i > 0 {
+		left = verifyPostorder(postorder[:i])
+	}
+
+	right := true
+	if i < len(postorder)-1 {
+		right = verifyPostorder(postorder[i : len(postorder)-1])
+	}
+
+	return left && right
+}
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
